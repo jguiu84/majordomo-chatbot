f8610562ae8f40d59b80f4db00d083ffe0351a55
mihai.stana@digitalvirgo.com <mihai.stana@digitalvirgo.com>
2024-01-25 10:55:04 +0100

    chat uiu

--- resources/views/livewire/chat-form.blade.php
+++ resources/views/livewire/chat-form.blade.php
@@ -1,35 +1,34 @@
 <div>
-    <div class="flex flex-col items-center justify-center w-screen min-h-screen bg-gray-100 text-gray-800 p-10">
+    <div class="flex flex-col items-center justify-center w-screen min-h-screen bg-slate-100 text-gray-800 p-10">
     <!-- Component Start -->
     <div class="flex flex-col flex-grow w-full max-w-xl bg-white shadow-xl rounded-lg overflow-hidden">
         <div class="flex flex-col flex-grow h-0 p-4 overflow-auto">
             @foreach ($messages as $message)
                 @if($message->is_bot_answer)
-                <div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
+                <div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end" wire:key="message-{{ $message->id }}">
                     <div>
                         <div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
                             <p class="text-sm">{{ $message->message }}</p>
                         </div>
-                        <span class="text-xs text-gray-500 leading-none">2 min ago</span>
+                        {{-- <span class="text-xs text-gray-500 leading-none">2 min ago</span> --}}
                     </div>
-                    <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+                    <div class="flex-shrink-0 h-10 w-10 rounded-full bg-slate-300"></div>
                 </div>
                 @else
-                <div class="flex w-full mt-2 space-x-3 max-w-xs">
-                    <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+                <div class="flex w-full mt-2 space-x-3 max-w-xs"  wire:key="message-{{ $message->id }}">
+                    <div class="flex-shrink-0 h-10 w-10 rounded-full bg-slate-300"></div>
                     <div>
-                        <div class="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
+                        <div class="bg-slate-300 p-3 rounded-r-lg rounded-bl-lg">
                             <p class="text-sm">{{ $message->message }}</p>
                         </div>
-                        <span class="text-xs text-gray-500 leading-none">2 min ago</span>
+                        {{-- <span class="text-xs text-gray-500 leading-none">2 min ago</span> --}}
                     </div>
                 </div>
                 @endif
             @endforeach
-            
         </div>
         
-        <div class="bg-gray-300 p-4">
+        <div class="bg-slate-300 p-4">
             <input class="flex items-center h-10 w-full rounded px-3 text-sm" type="text" placeholder="Type your message…" wire:model="newMessage" wire:keydown.enter="sendMessage">
         </div>
     </div>
